9e8730b7c19366efb577799cf6a6cca394f77857
none <none>
2018-02-09 21:09:37 -0800

    input keymappings

--- deviceConsole.py
+++ deviceConsole.py
@@ -1,9 +1,30 @@
 import sys
+import curses
 from device import device
 
 class Console(device):
 
 	stdscr = None
+
+	# VT100 and a smattering of later DEC encodings
+	input_keymap = {
+		10			: '\x0d',	# nl -> enter
+		127			: '\x08',	# del -> bs
+		curses.KEY_DOWN		: '\x1bB',	# Down-arrow
+		curses.KEY_UP		: '\x1bA',	# Up-arrow
+		curses.KEY_LEFT		: '\x1bD',	# Left-arrow
+		curses.KEY_RIGHT	: '\x1bC',	# Right-arrow
+		curses.KEY_HOME		: '\x1b1',	# Home key (upward+left arrow)
+		curses.KEY_F0		: '\x1bOP',	# Function keys. Up to 64 function keys are supported.
+		curses.KEY_F1		: '\x1bOQ',	# Function keys. Up to 64 function keys are supported.
+		curses.KEY_F2		: '\x1bOR',	# Function keys. Up to 64 function keys are supported.
+		curses.KEY_F3		: '\x1bOS',	# Function keys. Up to 64 function keys are supported.
+		curses.KEY_DC		: '\x08',	# Delete character
+		curses.KEY_IC		: '\x1b2',	# Insert char or enter insert mode
+		curses.KEY_NPAGE	: '\x1b6',	# Next page
+		curses.KEY_PPAGE	: '\x1b5',	# Previous page
+		curses.KEY_END		: '\x1b4'	# End
+	}
 
 	def __init__(self, address, interrupt, debug = False):
 		super(Console, self).__init__('console', debug = debug)
@@ -11,7 +32,6 @@ class Console(device):
 		Console.stdscr.nodelay(1)
 		Console.stdscr.scrollok(1)
 		Console.stdscr.idlok(1)
-
 
 	def _fmt(self, val):
 		str = '{}'.format(val)
@@ -24,6 +44,11 @@ class Console(device):
 			pass
 		return str
 
+	def _handle_input(self, input):
+		self.trace('in ' + self._fmt(input))
+		device.root_device.console_input(input)
+
+
 	def handle_console_output(self, output):
 		self.trace('out ' + self._fmt(output))
 		Console.stdscr.echochar(chr(output))
@@ -32,8 +57,10 @@ class Console(device):
 		Console.stdscr.refresh()
 		input = Console.stdscr.getch()
 		if input != -1:
-			# we want CR, not NL
-			if input == 10:
-				input = 13
-			self.trace('in ' + self._fmt(input))
-			device.root_device.console_input(input)
+
+			if input in Console.input_keymap:
+				self.trace('translate {} -> {}'.format(input, Console.input_keymap[input]))
+				for c in Console.input_keymap[input]:
+					self._handle_input(ord(c))
+			else:
+				self._handle_input(input)
