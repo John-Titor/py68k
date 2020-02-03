dea8d1acfc21f6338acec12c4e44041a255c3181
none <user@example.com>
2020-02-02 21:43:02 -0800

    add native features tests

--- tests/m68k_misc.h
+++ tests/m68k_misc.h
@@ -17,10 +17,11 @@ static inline uint32_t set_vbr() { uint32_t value; __asm__ volatile ("movec %0, 
 #endif
 
 typedef void    (*exception_handler_t)(void);
-#define VECTOR(_x)          (*(volatile exception_handler_t *)((_x) * 4 + VECTOR_BASE))
+#define VECTOR(_x)          (*((volatile exception_handler_t *)((_x) * 4 + VECTOR_BASE)))
 
+#define VEC_BUS_ERROR       VECTOR(2)           // Bus error
 #define VEC_ADDR_ERROR      VECTOR(3)           // Address error
-#define VEC_BUS_ERROR       VECTOR(4)           // Illegal instruction
+#define VEC_ILLEGAL         VECTOR(4)           // Illegal instruction
 #define VEC_DIV_ZERO        VECTOR(5)           // Zero divide
 #define VEC_CHK             VECTOR(6)           // CHK instruction
 #define VEC_TRAPV           VECTOR(7)           // TRAPV instruction
@@ -34,3 +35,107 @@ typedef void    (*exception_handler_t)(void);
 #define VEC_AUTOVECTOR(_y)  VECTOR(25 + (_y))   // Level 1 on-chip interrupt autovec
 #define VEC_TRAP(_y)        VECTOR(32 + (_y))   // TRAP instruction vectors
 #define VEC_USER(_y)        VECTOR(64 + (_y))   // User interrupt vectors
+
+static inline void
+zero_bss()
+{
+    extern uint32_t __bss_start;
+    extern uint32_t _end;
+    uint32_t *ptr = &__bss_start;
+
+    while (ptr < &_end) {
+        *ptr++ = 0;
+    }
+}
+
+extern bool _detect_native_features(void);
+extern uintptr_t _nfID(const char *);
+extern uint32_t _nfCall(uint32_t ID, ...);
+
+/*
+ * d0 - return code
+ * d1 - old illegal vector
+ * a0 - address of illegal vector
+ * a1 - old sp
+ */
+__asm__
+(
+#if defined(__mc68010) || defined(__mc68020) || defined(__mc68030) || defined(__mc68040)
+"        movec   %vbr, %a0                                              \n"
+"        add.l   #0x10, %a0                                             \n"
+#else
+"        move.l  #0x10, %a0                                             \n"
+#endif
+"_detect_native_features:                                               \n"
+"        moveq   #0,%d0              /* assume no NatFeats available */ \n"
+"        move.l  %sp,%a1                                                \n"
+"        move.l  (%a0),%d1                                              \n"
+"        move.l  #_fail_natfeat, (%a0)                                  \n"
+"        pea     _nf_version_name                                       \n"
+"        sub.l   #4,%sp                                                 \n"
+"        .dc.w   0x7300              /* Jump to NATFEAT_ID */           \n"
+"        tst.l   %d0                                                    \n"
+"        jeq     _fail_natfeat                                          \n"
+"        moveq   #1,%d0              /* NatFeats detected */            \n"
+"                                                                       \n"
+"_fail_natfeat:                                                         \n"
+"        move.l  %a1,%sp                                                \n"
+"        move.l  %d1,(%a0)                                              \n"
+"                                                                       \n"
+"        rts                                                            \n"
+"                                                                       \n"
+"_nf_version_name:                                                      \n"
+"        .ascii  \"NF_VERSION\\0\"                                      \n"
+"        .even                                                          \n"
+"                                                                       \n"
+"_nfID:                                                                 \n"
+"        .dc.w   0x7300                                                 \n"
+"        rts                                                            \n"
+"_nfCall:                                                               \n"
+"        .dc.w   0x7301                                                 \n"
+"        rts                                                            \n"
+);
+
+static uint32_t
+nfID(const char *method)
+{
+    static bool probed, supported;
+
+    if (!probed && _detect_native_features()) {
+        supported = true;
+    }
+    probed = true;
+    if (supported) {
+        return _nfID(method);
+    }
+    return 0;
+}
+
+static void
+nf_puts(const char *str)
+{
+    static uint32_t nfid_stderr = 0;
+
+    if (!nfid_stderr) {
+        nfid_stderr = nfID("NF_STDERR");
+    }
+
+    if (nfid_stderr) {
+        _nfCall(nfid_stderr, str);
+    }
+}
+
+static void
+nf_exit()
+{
+    static uint32_t nfid_shutdown = 0;
+
+    if (!nfid_shutdown) {
+        nfid_shutdown = nfID("NF_SHUTDOWN");
+    }
+
+    if (nfid_shutdown) {
+        _nfCall(nfid_shutdown);
+    }
+    for (;;) ;
+}
--- tests/simple.h
+++ tests/simple.h
@@ -4,22 +4,22 @@
 
 #include "m68k_misc.h"
 
-#define IO_BASE    0x00ff0000
+#define IO_BASE             0x00ff0000
 
-#define UART_BASE   (IO_BASE)
-#define TIMER_BASE  (IO_BASE + 0x1000)
+#define UART_BASE           (IO_BASE)
+#define TIMER_BASE          (IO_BASE + 0x1000)
 
-#define UART_STATUS     REG8(UART_BASE + 0x01)
-#define UART_STATUS_RXRDY   (0x01)
-#define UART_STATUS_TXRDY   (0x02)
-#define UART_DATA       REG8(UART_BASE + 0x03)
-#define UART_CONTROL    REG8(UART_BASE + 0x05)
-#define UART_CONTROL_RXIE   (0x01)
-#define UART_CONTROL_TXIE   (0x02)
-#define UART_VECTOR     REG8(UART_BASE + 0x07)
+#define UART_STATUS         REG8(UART_BASE + 0x01)
+#define UART_STATUS_RXRDY       (0x01)
+#define UART_STATUS_TXRDY       (0x02)
+#define UART_DATA           REG8(UART_BASE + 0x03)
+#define UART_CONTROL        REG8(UART_BASE + 0x05)
+#define UART_CONTROL_RXIE       (0x01)
+#define UART_CONTROL_TXIE       (0x02)
+#define UART_VECTOR         REG8(UART_BASE + 0x07)
 
-#define TIMER_PERIOD    REG32(TIMER_BASE + 0x00)
-#define TIMER_COUNT     REG32(TIMER_BASE + 0x04)
-#define TIMER_CONTROL   REG8(TIMER_BASE + 0x09)
-#define TIMER_CONTROL_IE    (0x01)
-#define TIMER_VECTOR    REG8(TIMER_BASE + 0x0b)
+#define TIMER_PERIOD        REG32(TIMER_BASE + 0x00)
+#define TIMER_COUNT         REG32(TIMER_BASE + 0x04)
+#define TIMER_CONTROL       REG8(TIMER_BASE + 0x09)
+#define TIMER_CONTROL_IE        (0x01)
+#define TIMER_VECTOR        REG8(TIMER_BASE + 0x0b)
