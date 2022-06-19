08f6d73fdf15519ba9857ec6ebcd5bdefbda0992
none <user@example.com>
2022-06-19 01:40:27 -0700

    simplify EEPROM loading for tiny68k

--- targets/tiny68k.py
+++ targets/tiny68k.py
@@ -18,7 +18,9 @@ def configure(args):
     emu = Emulator(args,
                    cpu='68000',
                    frequency=8 * 1000 * 1000)
-    emu.add_memory(base=0, size=(16 * 1024 - 32) * 1024)
+    emu.add_memory(base=0,
+                   size=(16 * 1024 - 32) * 1024,
+                   from_file=args.eeprom)
     emu.add_device(args,
                    MC68681,
                    address=0xfff001,
@@ -29,12 +31,5 @@ def configure(args):
                    CompactFlash,
                    address=0xffe000,
                    register_arrangement='16-bit')
-
-    if args.eeprom is not None:
-        rom_image = open(args.eeprom, 'rb').read(32 * 1024 + 1)
-        if (len(rom_image) > (32 * 1024)):
-            raise RuntimeError(f'EEPROM image {args.eeprom} must be <= 32k')
-        print(f'loaded {len(rom_image)} bytes of EEPROM')
-        m68k.mem_write_bulk(0, rom_image)
 
     return emu
